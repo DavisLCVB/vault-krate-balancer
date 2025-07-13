9078d797fe7876b71df36b8a86d86bb5c9732aeb
DavisLCVB <daviscvb22092004@gmail.com>
2025-07-13 01:07:26 -0500

    Update JSON and URL encoding limits in server.ts to use uppercase 'MB'

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,8 @@ import Proxy from "./proxy";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json({ limit: '100MB' }));
+app.use(express.urlencoded({ limit: '100MB', extended: true }));
 
 app.use(cors({
   origin: "*",
